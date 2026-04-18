45c8cd3401bf7b59dd3d7a866d77c69ab9ebbe05
USPARS001 <uspenskiyarseniy@gmail.com>
2026-04-18 19:06:00 +0200

    implemented findIndex in LPHashTable and QPHashTable

--- LPHashTable.java
+++ LPHashTable.java
@@ -1,4 +1,5 @@
 import java.util.List;
+
 /**
  * Simple hash table implementation using linear probing.
  *
@@ -10,20 +11,46 @@ public class LPHashTable extends HashTable {
     /**
      * Create an LPHashTable with DEFAULT_SIZE table.
      */
-    public LPHashTable() { super(); }
+    public LPHashTable() {
+        super();
+    }
 
     /**
      * Create an LPHashTable with the given default size table.
      */
-    public LPHashTable(final int size) { super(size); }
+    public LPHashTable(final int size) {
+        super(size);
+    }
 
     /**
-     * Find the index for entry: if entry is in the table, then returns its position;
+     * Find the index for entry: if entry is in the table, then returns its
+     * position;
      * if it is not in the table then returns the index of the first free slot.
      * Returns -1 if a slot is not found (such as when the table is full under LP).
      *
      */
     protected int findIndex(String key) {
-		// Implement using linear probing.
+        boolean firstLoop = true;
+        int hashed = hashFunction(key);
+        int target = hashed;
+        incProbeCount();
+        while (table[target] != null) {
+            if (key.compareTo(table[target]) == 0) {
+                return target;
+            }
+            target++;
+            if (target >= tableSize() || (!firstLoop && target >= hashed)) {
+                if (firstLoop) {
+                    firstLoop = false;
+                    target = 0;
+                    if (hashed == 0)
+                        return -1;
+                } else {
+                    return -1;
+                }
+            }
+            incProbeCount();
+        }
+        return target;
     }
 }
--- QPHashTable.java
+++ QPHashTable.java
@@ -4,26 +4,45 @@
  * @author Stephan Jamieson
  * @version 24/4/2015
  */
-public class QPHashTable  extends HashTable {
-
+public class QPHashTable extends HashTable {
 
     /**
      * Create an QPHashTable with DEFAULT_SIZE table.
      */
-    public QPHashTable() { super(); }
+    public QPHashTable() {
+        super();
+    }
 
     /**
      * Create an QPHashTable with the given default size table.
      */
-    public QPHashTable(final int size) { super(size); }
+    public QPHashTable(final int size) {
+        super(size);
+    }
 
     /**
-     * Find the index for entry: if entry is in the table, then returns its position;
+     * Find the index for entry: if entry is in the table, then returns its
+     * position;
      * if it is not in the table then returns the index of the first free slot.
      * Returns -1 if a slot is not found (such as when the table is full under LP).
      *
      */
     protected int findIndex(String key) {
-		// Implement using quadratic probing.
+        int hashed = hashFunction(key);
+        int target = hashed;
+        int probe = 1;
+        incProbeCount();
+        while (table[target] != null) {
+            if (key.compareTo(table[target]) == 0) {
+                return target;
+            }
+            target = (hashed + (probe * probe)) % tableSize();
+            if (probe >= tableSize()) {
+                return -1;
+            }
+            incProbeCount();
+            probe++;
+        }
+        return target;
     }
 }
